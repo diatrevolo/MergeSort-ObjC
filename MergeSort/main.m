01e0335953d8c75aff1d61910088eb1fa39a000a
Roberto Osorio <rjo218@gmail.com>
2015-10-07 12:03:26 -0400

    Initial Commit

---
+++ MergeSort/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MergeSort
+//
+//  Created by Roberto Osorio Goenaga on 10/7/15.
+//  Copyright © 2015 Roberto Osorio Goenaga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
